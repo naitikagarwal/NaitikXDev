0ecc0ce5c6d595f1f7cd5daf44d769eaefa46ed1
Naitik Kumar Agarwal <naitikagar12@gmail.com>
2024-08-28 01:01:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// function handleMenu(){
+//     navDialog.classList.toggle('hidden')   
+// }
+
+const handleMenu = () => {
+    const navDialog = document.getElementById('nav-dialog');
+    navDialog.classList.toggle('invisible')   ;
+}
+function notava(){
+    alert("Sorry! Data is not uploaded yet")
+}
+
+
+new WOW().init();
